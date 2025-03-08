5684b4045bad86a9264848b6cb56380ab8c5ce01
Roldin21 <136860341+21IRM21@users.noreply.github.com>
2025-03-08 16:01:50 +0000

    actualización connection.php

--- php/connection.php
+++ php/connection.php
@@ -6,10 +6,10 @@ $user = "root";
 $password = "";
 $db = ""; //Introduce el nombre de la base de datos
 
-$mysqli = new mysqli ($server, $user, $password, $db);
+$conexion = new mysqli ($server, $user, $password, $db);
 
-if ($mysqli->connect_errno) {
-    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+if ($conexion->connect_errno) {
+    echo "Falló la conexión a base de datos: (" . $conexion->connect_errno . ") " . $conexion->connect_error;
 }else {
     echo "Conexión establecida";
 }
